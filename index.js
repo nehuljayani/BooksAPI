188ec17f94d3b27ad88ec3397872f7a6f70ec0d8
nehuljayani <70128041+nehuljayani@users.noreply.github.com>
2021-02-05 17:39:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,21 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const route = require("./route");
 
-const app = express();
-app.get("",(req,res)=>{
-res.send("working");
-});
-app.listen(process.env.PORT || 3000,()=>{
-        console.log("server started")
-})
-//mongoose.connect("mongodb+srv://nj:12345@cluster0.r1bv4.mongodb.net/Students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
-//()=>{
-  //      const app = express();
-    //    app.use("/api",route);
-      //  app.use(bodyParser.urlencoded({extended: true}));
-        //app.use(express.json());
-        //app.listen(process.env.PORT || 3000,()=>{
-          //  console.log("Server Started on 3000 !!");
-       // });
-    //}
-//);
+mongoose.connect("mongodb+srv://nj:12345@cluster0.r1bv4.mongodb.net/Students?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+()=>{
+        const app = express();
+        app.use("/api",route);
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(express.json());
+        app.listen(process.env.PORT || 3000,()=>{
+            console.log("Server Started on 3000 !!");
+        });
+    }
+);
